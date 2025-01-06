588a1c5d733f85b43051129e0cce012cd9f833ae
Jorge Gasco <97962858+JorGasco@users.noreply.github.com>
2025-01-06 01:30:52 +0000

    Added routing for Updating and Deleting a Task

--- node-lab1/tasky-api/eslint.config.mjs
+++ node-lab1/tasky-api/eslint.config.mjs
@@ -7,13 +7,13 @@ export default [
     files: ["**/*.js"],
     languageOptions: {
       globals: globals.node,
-      ecmaVersion: 2015, // ES6
+      ecmaVersion: 2020, // Updated to fully support ES2020 (or use "latest")
       sourceType: "module", // Use module format
     },
     rules: {
-      "semi": 1,
-      "no-console": "off",
+      "semi": 1, // Warn for missing semicolons
+      "no-console": "off", // Allow console.log statements
     },
   },
-  pluginJs.configs.recommended,
+  pluginJs.configs.recommended, // Apply recommended ESLint rules
 ];
